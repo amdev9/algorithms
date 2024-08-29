c61001c2b796852a9df8e1c671f3a6e9280a3d1c
amdev9 <surfera@Alexs-MacBook-Pro.local>
2024-08-30 01:08:01 +0300

    add avl tree implementation

---
+++ tree/redBlackTree/1.js
@@ -0,0 +1,4 @@
+// https://github.com/trekhleb/javascript-algorithms/tree/master/src/data-structures/tree/red-black-tree
+// https://www.geeksforgeeks.org/insertion-in-red-black-tree/
+
+// https://www.youtube.com/watch?v=UaLIHuR1t8Q&list=PLLXdhg_r2hKA7DPDsunoDZ-Z769jWn4R8&index=13
